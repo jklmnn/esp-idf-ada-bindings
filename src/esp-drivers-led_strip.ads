b79f001625930cf8653e150115c6f699745b4f56
Johannes Kliemann <jk@jkliemann.de>
2023-04-15 19:43:10 +0200

    Update LED strip driver to ESP-IDF 5.0.1

--- src/esp-drivers-led_strip.ads
+++ src/esp-drivers-led_strip.ads
@@ -1,5 +1,6 @@
 with System;
 with Interfaces;
+with Interfaces.C;
 with ESP.Error;
 with ESP.Drivers.GPIO;
 
@@ -8,14 +9,54 @@ is
 
    type Strip is new System.Address;
 
+   type LED_Pixel_Format is (GRB, GRBW, Format_INVALID) with
+      Size => 32;
+
+   for LED_Pixel_Format use (GRB     => 0,
+                             GRBW    => 1,
+                             Format_INVALID => 2);
+
+   type LED_Model is (WS2812, SK6812, Model_INVALID) with
+      Size => 32;
+
+   for LED_Model use (WS2812        => 0,
+                      SK6812        => 1,
+                      Model_INVALID => 2);
+
+   type LED_Config_Flags is record
+      Invert_Out : Boolean;
+   end record with
+      Size => 32;
+
+   for LED_Config_Flags use record
+      Invert_Out at 0 range 0 .. 0;
+   end record;
+
    type LED_Config is record
       GPIO_Num : GPIO.Num;
       Max_Leds : Interfaces.Unsigned_32;
+      Format   : LED_Pixel_Format := GRB;
+      Model    : LED_Model        := WS2812;
+      Flags    : LED_Config_Flags := LED_Config_Flags'(Invert_Out => False);
+   end record with
+      Convention => C;
+
+   type RMT_Config_Flags is record
+      With_DMA : Boolean;
+   end record with
+      Size => 32;
+
+   for RMT_Config_Flags use record
+      With_DMA at 0 range 0 .. 0;
    end record;
 
    type RMT_Config is record
-      Resolution : Interfaces.Unsigned_32;
-   end record;
+      Clock_Source      : Interfaces.C.int;
+      Resolution        : Interfaces.Unsigned_32;
+      Mem_Block_Symbols : Interfaces.C.size_t;
+      Flags             : RMT_Config_Flags;
+   end record with
+      Convention => C;
 
    function New_RMT_Device (L :     access LED_Config;
                             R :     access RMT_Config;
